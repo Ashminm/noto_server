df591741280e9dfb4cf18a375e3a991e76d1ce93
Ashmin <ashmin2802@gmail.com>
2024-10-01 23:09:14 +0530

    ch

--- Connection/db.js
+++ Connection/db.js
@@ -1,9 +1,20 @@
-const mongoose=require('mongoose')
 
-const connectionString=process.env.DATABASE
-mongoose.connect(connectionString).then((res)=>{
-    console.log("Connection Established.(Atles)!!");
-}).catch((err)=>{
-    console.log(err);
-    console.log("Internet Connection Faild.  Your Offline!!");
+const mongoose = require('mongoose');
+
+const connectionString = process.env.DATABASE;
+
+mongoose.connect(connectionString, {
+    serverSelectionTimeoutMS: 10000, 
 })
+.then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+})
+.catch((err) => {
+    if (err.message && err.message.includes("querySrv ENOTFOUND")) {
+        console.error("Connection error: Unable to resolve MongoDB Atlas host. Please check your connection string.");
+    } else if (err.message && err.message.includes("cluster is paused")) {
+        console.error("Connection error: The MongoDB Atlas cluster is paused. Please resume the cluster to allow connections.");
+    } else {
+        console.error("Connection error:", err);
+    }
+});
